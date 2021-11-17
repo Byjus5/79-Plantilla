8f6e709585e5cfb7b91771572b56c33665b38ce9
Byjus5 <89545408+Byjus5@users.noreply.github.com>
2021-11-17 13:49:25 -0600

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -32,8 +32,8 @@ export default class RideScreen extends Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
     this.setState({
-      /*status === "granted" is true when user has granted permission
-          status === "granted" is false when user has not granted the permission
+      /*status === "granted" es "true" cuando el usuario ha dado permiso
+          status === "granted" es "false" cuando el usuario no ha dado permiso
         */
       hasCameraPermissions: status === "granted",
       domState: "scanner",
@@ -65,11 +65,11 @@ export default class RideScreen extends Component {
   };
 
   assignBike = () => {
-    console.log("You have rented the bike for next 1 hour. Enjoy your ride!!");
+    console.log("Has rentado la bicicleta por la siguiente 1 hora. ¡Disfruta tu viaje!");
   };
 
   returnBike = () => {
-    console.log("We hope you enjoyed your ride");
+    console.log("Esperamos que hayas disfrutado tu viaje");
   };
 
   render() {
@@ -86,8 +86,8 @@ export default class RideScreen extends Component {
       <View style={styles.container}>
         <View style={styles.upperContainer}>
           <Image source={appIcon} style={styles.appIcon} />
-          <Text style={styles.title}>e-ride</Text>
-          <Text style={styles.subtitle}>A Eco-Friendly Ride</Text>
+          <Text style={styles.title}>Travesía digital</Text>
+          <Text style={styles.subtitle}>Un viaje eco-friendly</Text>
         </View>
         <View style={styles.lowerContainer}>
           <View style={styles.textinputContainer}>
@@ -110,14 +110,14 @@ export default class RideScreen extends Component {
               style={styles.scanbutton}
               onPress={() => this.getCameraPermissions()}
             >
-              <Text style={styles.scanbuttonText}>Scan</Text>
+              <Text style={styles.scanbuttonText}>Escanear</Text>
             </TouchableOpacity>
           </View>
           <TouchableOpacity
             style={[styles.button, { marginTop: 25 }]}
             onPress={this.handleTransaction}
           >
-            <Text style={styles.buttonText}>Unlock</Text>
+            <Text style={styles.buttonText}>Desbloquear</Text>
           </TouchableOpacity>
         </View>
       </View>
